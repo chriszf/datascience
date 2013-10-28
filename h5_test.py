9040a524ba14d8445002471c44e233681df77324
chriszf <chriszf@gmail.com>
2013-10-27 20:10:28 -0700

    removing the original getters file

--- h5_test.py
+++ h5_test.py
@@ -1,3 +1,3 @@
-import hdf5_getters
+import tables
 filename = "PATH/TO/MSD/MillionSongSubset/data/A/A/A/TRAAAAW128F429D538.h5"
-h5 = hdf5_getters.open_h5_file_read()
+h5 = tables.openFile(filename, mode="r")
