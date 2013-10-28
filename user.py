28fbda44d6f8baf7927843d5d38634781bcd7505
chriszf <chriszf@gmail.com>
2013-10-28 13:44:53 -0700

    Fixing user.py

--- user.py
+++ user.py
@@ -7,16 +7,21 @@ def main():
     last_user_id = None
     users = client.datascience.users
     users.remove() # Erase the existing collection
+    user = None
 
     for line in f:
         user_id, song_id, plays = line.split()
         plays = int(plays)
         if user_id != last_user_id:
+            if user:
+                users.insert(user)
+
             user = {"user_id": user_id, songs = []}
         user['songs'].append({"song_id": song_id,
                               "plays": plays})
 
-        users.insert(user)
+    user.insert(user)
+
 
 if __name__ == "__main__":
     main()
