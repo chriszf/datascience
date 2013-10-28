dd6a741f21ab238dd2302c956ba68a9496b08971
chriszf <chriszf@gmail.com>
2013-10-27 22:57:25 -0700

    Adding user.py solution

--- user.py
+++ user.py
@@ -6,12 +6,16 @@ def main():
     f = open("train_triplets.txt")
     last_user_id = None
     users = client.datascience.users
-    for line in f:
-        user_id, song, plays = line.split()
-        if user_id != last_user_id:
-            pass
 
-        # insert a record into the users collection
+    for line in f:
+        user_id, song_id, plays = line.split()
+        plays = int(plays)
+        if user_id != last_user_id:
+            user = {"user_id": user_id, songs = []}
+        user['songs'].append({"song_id": song_id,
+                              "plays": plays})
+
+        users.insert(user)
 
 if __name__ == "__main__":
     main()
